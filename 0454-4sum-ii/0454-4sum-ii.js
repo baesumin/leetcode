96cbe6d6927e1ba5ca859f50fd4e91ec09592f02
SM <52240990+baesumin@users.noreply.github.com>
2025-01-21 22:19:15 +0900

    Time: 130 ms (45.71%), Space: 55.8 MB (25%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function (nums1, nums2, nums3, nums4) {
+  let map = new Map()
+  let cnt = 0
+
+  for (let num1 of nums1) {
+    for (let num2 of nums2) {
+      map.set(num1 + num2, (map.get(num1 + num2) || 0) + 1)
+    }
+  }
+
+  for (let num3 of nums3) {
+    for (let num4 of nums4) {
+      if (map.has(-num3 - num4)) cnt += map.get(-num3 - num4)
+    }
+  }
+
+  return cnt
+};
+
