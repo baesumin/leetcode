433ad34a6c6ae78982b93291ec96ec4892dea897
SM <52240990+baesumin@users.noreply.github.com>
2025-01-22 21:39:31 +0900

    Time: 15 ms (92.52%), Space: 54.5 MB (24.65%) - LeetHub

--- 0018-4sum/0018-4sum.js
+++ 0018-4sum/0018-4sum.js
@@ -5,33 +5,30 @@
  */
 var fourSum = function (nums, target) {
   nums.sort((a, b) => a - b)
-  console.log(nums)
   let result = []
-
-  for (let i = 0; i < nums.length - 3; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
-    for (let j = i + 1; j < nums.length - 2; j++) {
-      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
-
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       let left = j + 1
       let right = nums.length - 1
 
       while (left < right) {
         let sum = nums[i] + nums[j] + nums[left] + nums[right]
+
         if (sum === target) {
           result.push([nums[i], nums[j], nums[left], nums[right]])
-          while (left < right && nums[left + 1] === nums[left]) left++;
-          while (left < right && nums[right - 1] === nums[right]) right--;
-          left++;
-          right--;
+          while (nums[left] === nums[left + 1]) left++
+          while (nums[right] === nums[right + 1]) right--
+          left++
+          right--
         } else if (sum > target) {
           right--
         } else {
           left++
         }
       }
+      while (nums[j] === nums[j + 1]) j++
     }
+    while (nums[i] === nums[i + 1]) i++
   }
-
   return result
-}
+};
