8ef13e09cfb96a92fc7c8a346363b9e4dce2d5f3
SM <52240990+baesumin@users.noreply.github.com>
2025-01-23 08:39:26 +0900

    Time: 25 ms (7.44%), Space: 52.1 MB (24.37%) - LeetHub

---
+++ 0139-word-break/0139-word-break.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {
+  let set = new Set(wordDict)
+  let seen = new Set()
+  let q = [[0, '']]
+
+  while (q.length) {
+    let [pos] = q.shift()
+    if (pos === s.length) return true
+
+    let word = ''
+    for (let i = pos; i <= s.length; i++) {
+      if (set.has(word) && !seen.has(i)) {
+        seen.add(i)
+        q.push([i])
+      }
+      word += s[i]
+    }
+  }
+
+  return false
+};
