ea2de2e8ecf6093bd68619ac22327e5488d7eeeb
SM <52240990+baesumin@users.noreply.github.com>
2025-01-22 21:19:39 +0900

    Time: 59 ms (22.83%), Space: 65.9 MB (59.37%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,30 +3,32 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  const result = []
   nums.sort((a, b) => a - b)
-
-  for (let target = 0; target < nums.length-2; target++) {
-    const currentTarget = nums[target]
+  console.log(nums)
+  let result = []
+  for (let target = 0; target < nums.length; target++) {
     let left = target + 1
     let right = nums.length - 1
 
     while (left < right) {
+
       let sum = nums[target] + nums[left] + nums[right]
+
       if (sum === 0) {
-        result.push([currentTarget, nums[left], nums[right]])
-        while (nums[left + 1] === nums[left]) left++;
-        while (nums[right - 1] === nums[right]) right--;
-        left++;
-        right--;
-      } else if (sum > 0) {
+        result.push([nums[target], nums[left], nums[right]])
+        while (nums[left] === nums[left + 1]) left++
+        while (nums[right] === nums[right + 1]) right--
+        left++
+        right--
+      }
+      else if (sum > 0) {
         right--
       } else {
         left++
       }
     }
-    while (nums[target + 1] === nums[target]) target++;
-  }
 
+    while (target < nums.length - 1 && nums[target] === nums[target + 1]) target++
+  }
   return result
-};  
+};
