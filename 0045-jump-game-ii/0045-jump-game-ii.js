29c64413a7d10e77bf429b437b84a5f3ccf5c982
SM <52240990+baesumin@users.noreply.github.com>
2025-02-02 17:29:54 +0900

    Time: 138 ms (12.19%), Space: 50.9 MB (74.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -1,21 +1,18 @@
-function jump(nums) {
-    let jumps = 0;
-    let left = 0;
-    let right = 0;
-    let farthest = 0;
-    
-    // 마지막 인덱스 직전까지만 순회
-    while (right < nums.length - 1) {
-        // 현재 범위 내에서 가장 멀리 갈 수 있는 위치 찾기
-        for (let i = left; i <= right; i++) {
-            farthest = Math.max(farthest, i + nums[i]);
-        }
-        
-        // 다음 범위 설정
-        left = right + 1;
-        right = farthest;
-        jumps++;
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+  let dp = Array(nums.length).fill(0)
+
+  for (let i = 0; i < nums.length; i++) {
+    let v = nums[i]
+    for (let j = 1; j <= v; j++) {
+      if (dp[i + j] === 0) {
+        dp[i + j] = dp[i] + 1
+      }
     }
-    
-    return jumps;
-}
+  }
+
+  return dp[nums.length - 1]
+};
