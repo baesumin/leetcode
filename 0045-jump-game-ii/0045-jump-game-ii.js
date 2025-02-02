f8375674649d9ccf476722ac6bbf720630df9905
SM <52240990+baesumin@users.noreply.github.com>
2025-02-02 16:52:54 +0900

    Time: 141 ms (11.8%), Space: 51.9 MB (14.07%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let dp = Array(nums.length).fill(0)
+    
+    for(let i=0;i<nums.length;i++){
+      let v = nums[i]
+      for(let j=1;j<=v;j++){
+        if(dp[i+j]===0){
+          dp[i+j] = dp[i]+1
+        }
+      }
+    }
+
+    return dp[nums.length-1]
+};
