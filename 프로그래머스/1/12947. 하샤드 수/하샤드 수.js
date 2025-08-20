b595a6a330ca4234a615aed4c1ecb718d1cb964e
SM <52240990+baesumin@users.noreply.github.com>
2025-08-20 09:24:40 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,6 +1,4 @@
 function solution(x) {
     let digitSum = String(x).split('').map(Number).reduce((acc,cur)=>acc+cur)
-    if(x%digitSum===0) return true
-    
-    return false;
+    return x%digitSum===0;
 }
