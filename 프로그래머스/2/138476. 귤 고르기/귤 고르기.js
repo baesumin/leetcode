abd9436ad62d0fceb11e1a2dc4e8644464e3acf8
SM <52240990+baesumin@users.noreply.github.com>
2025-08-25 09:48:21 +0900

    [level 2] Title: 귤 고르기, Time: 80.92 ms, Memory: 57 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,25 @@
+function solution(k, tangerine) {
+    tangerine.sort((a,b)=>a-b)
+    let map = new Map()
+    let remainK = k
+    
+    for(let i=0;i<tangerine.length;i++){
+        let cur = tangerine[i]
+        map.set(cur,map.get(cur)+1||1)
+    }
+    
+    let arr = [...map].sort((a,b)=>b[1]-a[1])
+    let cnt = 1
+    
+    for(let i=0;i<arr.length;i++){
+        let remain = remainK-arr[i][1]
+        if(remain<=0){
+            break
+        }else{
+            remainK = remain
+            cnt++
+        }
+    }
+    
+    return cnt
+}
