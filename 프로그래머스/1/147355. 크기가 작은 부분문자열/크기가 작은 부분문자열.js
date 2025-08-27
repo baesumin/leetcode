2353e7f0d53b84ca27f0339ca665934932462e4a
SM <52240990+baesumin@users.noreply.github.com>
2025-08-28 08:46:37 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 353.06 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,10 @@
+function solution(t, p) {
+    var answer = 0;
+    
+    for(let i=0;i<=t.length-p.length;i++){
+        let str = t.slice(i,i+p.length)
+        if(+str<=+p) answer++
+    }
+    
+    return answer;
+}
