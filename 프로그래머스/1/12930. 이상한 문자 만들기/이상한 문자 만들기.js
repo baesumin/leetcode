4011a029e37decf8e9f7408da5593a4b301080c7
SM <52240990+baesumin@users.noreply.github.com>
2025-08-28 09:22:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 3.29 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    var answer = '';
+    let cnt = 0
+    
+    for(let i=0;i<s.length;i++){
+        if(s[i]===' ')cnt=-1
+        if(cnt%2===0)answer+=s[i].toUpperCase()
+        else answer+=s[i].toLowerCase()
+        cnt++
+    }
+    
+    console.log(answer)
+    
+    return answer;
+}
