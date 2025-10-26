e56d17573ae4edce538641acc3714200d2d70a17
SM <52240990+baesumin@users.noreply.github.com>
2025-10-26 11:56:33 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 188.48 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,26 +1,25 @@
 function solution(queue1, queue2) {
-    let sum1 = queue1.reduce((a,b)=>a+b)
-    let sum2 = queue2.reduce((a,b)=>a+b)
-    let sum = sum1 + sum2
-    let count = 0
-    let limit = (queue1.length + queue2.length) * 2;
-    let i1=0,i2=0
+    const queue = [...queue1, ...queue2];
+    let sum1 = queue1.reduce((a, b) => a + b, 0);
+    const target = (sum1 + queue2.reduce((a, b) => a + b, 0)) / 2;
     
-    if(sum%2!==0) return -1
+    let start = 0;
+    let end = queue1.length;
+    let count = 0;
+    const maxCount = queue.length * 2;
     
-    let arr = [...queue1, ...queue2];
-    
-    while(count<=limit){
-        if(sum1===sum/2) return count
-        if(sum1>sum/2){
-            sum1-=arr[i1]
-            i1++            
-        }else{
-            sum1+=arr[(i2 + queue1.length) % arr.length]
-            i2++
+    while (count < maxCount) {
+        if (sum1 === target) return count;
+        
+        if (sum1 > target) {
+            sum1 -= queue[start];
+            start++;
+        } else {
+            sum1 += queue[end];
+            end++;
         }
-        count++
+        count++;
     }
     
-    return -1
+    return -1;
 }
